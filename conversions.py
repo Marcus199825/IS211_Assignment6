f2271b33aaa8a5e56e58608b8fc7e82e9624b755
Marcus199825 <157505839+Marcus199825@users.noreply.github.com>
2024-03-03 19:43:52 -0500

    update

--- conversions.py
+++ conversions.py
@@ -1,13 +1,13 @@
 
 def convertCelsiusToKelvin(celsius):
     """Takes in a float representing a Celsius measurement, and returns that temperature converted into Kelvins"""
-    kelvins = 0
+    kelvins = 0.0
     
     return kelvins
 
 
 def convertCelsiusToFahrenheit(celsius):
     """Takes in a float representing a Celsius measurement, and returns that temperature converted into Fahrenheit"""
-    fahrenheit = 0
+    fahrenheit = 0.0
     
     return fahrenheit
